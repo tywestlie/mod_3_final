f16a4cf6d3e318889d9722064dc808d1ae0c7e41
Tyler Westlie <tyler.westlie@gmail.com>
2018-07-25 11:42:36 -0600

    recives expected GET after POSTing user id and a word

--- app/controllers/api/v1/games/plays_controller.rb
+++ app/controllers/api/v1/games/plays_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::Games::PlaysController < ApplicationController
   def create
-    play = Play.create(user_id: params[:user_id], word: params[:word], game_id: params[:id])
+    play = Play.new(user_id: params[:user_id], word: params[:word], game_id: params[:id])
+    if play.save
+      render json: {message: "play created"}, status: :created
+    end
   end
 end
